1fc04d530016e9f49a864e22ca9b477cdfa5d020
Eliasstl <ejmotoboy1@gmail.comn>
2021-03-04 20:57:02 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Criar o arquivo package
+npm init
+
+//Gerencia as requisições, rotas e URls, entre outra funcionalidades
+
+npm install express
+
+
+//Instalar o módulo para reiniciar o servidor sempre que houver alteração no código fonte
+
+npm install -D nodemon
+
+--------------------------------------------------------------
+COMO RODAR O PROJETO BAIXADO
+--------------------------------------------------------------
+
+//Instalar todas as dependências indicada pelo package.json
+
+npm install
+
+// Rodar projeto usando o nodemon
+
+nodemon app.js
+
+
+Instalar o MongoDB
+npm install --save  mongodb
+
+//Instalar o Mongoose - Mongoose traduz os dados do banco de dados para objetos
+javascript para que possam ser ultilizados por sua aplicação
+
+npm install --save mongoose 
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("./models/Artigo");
+
+const Artigo = mongoose.model('artigo')
+
+const app = express();
+
+app.use(express.json());
+
+
+ mongoose.connect('mongodb://localhost/EliasApi',{
+     useNewUrlParser:true,
+     useUnifiedTopology:true
+ }).then(()=>{
+     console.log("Conexão com MongoDB realizada com sucesso! ")
+ }).catch((erro)=>{
+    console.log("Error: Conexão com MongoDB não foi realizada com sucesso! ")
+ });
+
+app.get("/", (req, res)=>{
+    return res.json({titulo: "Como criar API'"})
+});
+
+app.post("/artigo",(req, res)=>{
+    const artigo = Artigo.create(req.body,(err)=>{
+        if(err) return res.status(400).json({
+            error:true,
+            message:"Error artigo não foi cadastrado com sucesso!"
+        })
+
+        return res.status(200).json({
+            error:false,
+            message:"Artigo foi cadastrado com sucesso!"
+        })
+
+
+    })
+})
+
+
+app.listen(8080,()=>{
+    console.log("servidor iniciado na porta 8080: http://localhost:8080");
+})
